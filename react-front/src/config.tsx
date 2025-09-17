79be8ed27a33c8037b506f57e1e3f0cdf6c7f986
Xi Luo <429066012@qq.com>
2025-09-17 17:13:38 +1000

    Update config apiURL

--- react-front/src/config.tsx
+++ react-front/src/config.tsx
@@ -1,8 +1,6 @@
 // src/config.tsx
-const isProduction = import.meta.env && import.meta.env.PROD;
-
 const config = {
-    apiUrl: isProduction ? 'https://crudapi.xiluo.net' : 'http://localhost:3000/api',
+    apiUrl: import.meta.env.PROD ? 'https://crudapi.xiluo.net/api' : 'http://localhost:3000/api',
     apiType: 'restapi', // 'restapi' or 'graphql'
   };
   
