c414c8d70024b23d0e25e4f4dd56eef6c58039ac
luoxisteven <429066012@qq.com>
2025-11-18 07:02:27 +1100

    update list mongodb

--- serverless-mongodb/list.py
+++ serverless-mongodb/list.py
@@ -2,6 +2,7 @@ import json
 import os
 from pymongo import MongoClient, ASCENDING, DESCENDING
 from bson.objectid import ObjectId
+from urllib.parse import quote_plus
 
 
 _mongo_client = None
@@ -10,9 +11,19 @@ _mongo_client = None
 def _get_db():
     global _mongo_client
     if _mongo_client is None:
-        mongo_url = os.environ.get("MONGO_URL") or os.environ.get("DB_HOST")
+        mongo_url = os.environ.get("MONGO_URL")
         if not mongo_url:
-            raise RuntimeError("Missing MONGO_URL")
+            host = os.environ.get("MONGO_HOST") or os.environ.get("DB_HOST") or "localhost"
+            port = os.environ.get("MONGO_PORT", "27017")
+            user = os.environ.get("MONGO_USER")
+            password = os.environ.get("MONGO_PASSWORD")
+            auth_source = os.environ.get("MONGO_AUTH_SOURCE", "admin")
+            if user and password:
+                user_enc = quote_plus(user)
+                pass_enc = quote_plus(password)
+                mongo_url = f"mongodb://{user_enc}:{pass_enc}@{host}:{port}/?authSource={auth_source}"
+            else:
+                mongo_url = f"mongodb://{host}:{port}"
         _mongo_client = MongoClient(mongo_url)
     db_name = os.environ.get("MONGO_DB_NAME") or os.environ.get("DB_NAME") or "test"
     return _mongo_client[db_name]
